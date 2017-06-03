1e6bc1da57a5a41ec73c760c190ffb486e5b51f4
my88899 <haojiutao@gmail.com>
2017-06-03 09:05:12 +0800

    test for gradle style

--- src/main/java/org/coach/tdd/template/Library.java
+++ src/main/java/org/coach/tdd/template/Library.java
@@ -1,7 +1,24 @@
 package org.coach.tdd.template;
+import java.lang.String;
 
 public class Library {
     public boolean someLibraryMethod() {
         return true;
+    }
+    public String sometoString(int num) {
+        String result = "";
+        result = gemThree(num) + gemFive(num);
+//        if(num % 15 == 0){result = "BizzPizz";}
+//        else if(num % 5 == 0){result = "Pizz";}
+//        else if(num % 3 == 0){result = "Bizz";}
+        return result;
+    }
+
+    private String gemThree(int num) {
+        return num % 3 == 0 ? "Bizz" : "";
+    }
+
+    private String gemFive(int num) {
+        return num % 5 == 0 ? "Pizz" : "";
     }
 }
--- src/test/java/org/coach/tdd/template/LibraryTest.java
+++ src/test/java/org/coach/tdd/template/LibraryTest.java
@@ -1,13 +1,28 @@
 package org.coach.tdd.template;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+
 
 import org.junit.Test;
 
 public class LibraryTest {
     @Test
     public void shouldSomeLibraryMethodReturnTrue() {
+
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+    @Test
+    public void testReadyForTestString(){
+        Library classUnderTest = new Library();
+        assertEquals(classUnderTest.sometoString(3), "Bizz");
+    }
+    @Test
+    public void testReadyForTestSingleNumberRight(){
+        Library classUnderTest = new Library();
+        assertEquals(classUnderTest.sometoString(3),"Bizz");
+        assertEquals(classUnderTest.sometoString(5),"Pizz");
+        assertEquals(classUnderTest.sometoString(30),"BizzPizz");
     }
 }
